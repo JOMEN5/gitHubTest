c7b12b7dd19cf3107ff0e66142720068795de282
陈晨 <744789621@qq.com>
2016-01-04 10:57:54 +0800

    Xcode with github use test

---
+++ gitHubTest/gitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitHubTest
+//
+//  Created by JOMEN on 16/1/4.
+//  Copyright © 2016年 Chenchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitHubTest/gitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitHubTest
+//
+//  Created by JOMEN on 16/1/4.
+//  Copyright © 2016年 Chenchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
